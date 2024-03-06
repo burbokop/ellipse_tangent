7b99b9b816758e5468a299bb782073c2d711a5b1
Borys Boiko <burbokop@gmail.com>
2024-03-07 00:59:46 +0200

    android ci

--- scripts/run-on-android.sh
+++ scripts/run-on-android.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -11,5 +11,10 @@ EXE="$1"
 EXE_NAME=`basename $EXE`
 adb push "$EXE" "/data/local/tmp/$EXE_NAME"
 adb shell "chmod 755 /data/local/tmp/$EXE_NAME"
-OUT="$(mktemp)"
-adb shell "RUST_BACKTRACE=full RUST_LOG=debug /data/local/tmp/$EXE_NAME $2" 2>&1 | tee $OUT
+
+cmd="RUST_BACKTRACE=full RUST_LOG=debug /data/local/tmp/$EXE_NAME $2"
+out=`adb shell "$cmd && echo SUCCESS || echo FAIL"`
+echo "$out"
+if [[ `echo "$out" | tail -n1` =~ ^FAIL  ]]; then
+    exit 1
+fi
