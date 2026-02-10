c7cb18d759d61d6512ccc718f9fb1ab06e318e30
Borys Boiko <burbokop@gmail.com>
2026-02-10 22:33:23 +0100

    Gravity simulation added

---
+++ examples/gravity_simulator/src/font_provider.rs
@@ -0,0 +1,40 @@
+use font_loader::system_fonts;
+use nannou::text::Font;
+
+pub struct FontProvider {
+    bytes: Vec<u8>,
+}
+
+fn choose_font(fonts: Vec<String>) -> String {
+    // Fonts with symbol ₴
+    let preferred_fonts = ["DejaVu Sans Mono", "FreeMono", "FreeSans Mono"];
+
+    for font in preferred_fonts {
+        if fonts.contains(&font.into()) {
+            return font.into();
+        }
+    }
+
+    fonts.first().unwrap().into()
+}
+
+impl FontProvider {
+    pub fn new() -> Self {
+        let mut property = system_fonts::FontPropertyBuilder::new().monospace().build();
+        let sys_fonts = system_fonts::query_specific(&mut property);
+        let font_bytes = system_fonts::get(
+            &system_fonts::FontPropertyBuilder::new()
+                .family(&choose_font(sys_fonts))
+                .build(),
+        )
+        .unwrap();
+
+        Self {
+            bytes: font_bytes.0,
+        }
+    }
+
+    pub fn font(&'static self) -> Font {
+        Font::from_bytes(&self.bytes).unwrap()
+    }
+}
---
+++ examples/gravity_simulator/src/md_array.rs
@@ -0,0 +1,75 @@
+use std::ops::Div;
+
+use nannou::math::num_traits::real::Real;
+
+#[derive(Debug)]
+pub struct MdArray<T, const D: usize> {
+    data: Vec<T>,
+    width: usize,
+    height: usize,
+}
+
+impl<T, const D: usize> MdArray<T, D> {
+    pub fn new(v: T, width: usize, height: usize) -> Self
+    where
+        T: Clone,
+    {
+        Self {
+            data: vec![v; width * height],
+            width,
+            height,
+        }
+    }
+
+    pub fn raw(&self) -> &[T] {
+        &self.data
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    pub fn at(&self, x: usize, y: usize) -> &T {
+        &self.data[y * self.width + x]
+    }
+
+    pub fn at_mut(&mut self, x: usize, y: usize) -> &mut T {
+        &mut self.data[y * self.width + x]
+    }
+
+    pub fn normalized(self) -> Self
+    where
+        T: Ord,
+        T: Div<Output = T>,
+        T: Clone,
+    {
+        let max = self.data.iter().max().unwrap().clone();
+        Self {
+            data: self.data.into_iter().map(|x| x / max.clone()).collect(),
+            width: self.width,
+            height: self.height,
+        }
+    }
+
+    pub fn partially_normalized(self) -> Self
+    where
+        T: PartialOrd,
+        T: Div<Output = T>,
+        T: Clone,
+        T: Real,
+    {
+        let max = *self
+            .data
+            .iter()
+            .max_by(|a, b| a.abs().partial_cmp(&b.abs()).unwrap())
+            .unwrap();
+        Self {
+            data: self.data.into_iter().map(|x| x / max).collect(),
+            width: self.width,
+            height: self.height,
+        }
+    }
+}
